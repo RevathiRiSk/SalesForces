1891bfb63e54abd95c7df7b0e9fded667de79158
RevathiRiSk <140264157+RevathiRiSk@users.noreply.github.com>
2023-10-03 17:27:58 -0400

    Add files via upload

---
+++ src/main/java/testCases/DeleteAccount.java
@@ -0,0 +1,27 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.AccountsPage;
+import pages.AppLauncherPage;
+import pages.SalesHomePage;
+
+public class DeleteAccount extends BaseTest{
+
+	
+	@Test
+	public void TC003_deleteAccountTest() throws InterruptedException {
+		
+		String accountName = "Duplicate";
+/*		LoginPage loginP = LoginPage.initialize(driver);
+		HomePage homeP = loginP.loginWithValidCredentials("revmohan@gmail.com", "Mohanrevathi1!");
+*/		AppLauncherPage appP = homeP.clickOnToggle().clickOnViewAll();
+		SalesHomePage salesP = appP.clickOnSales();
+		AccountsPage accP = salesP.clickOnAccounts().inputInSearch(accountName).clickDropDown().clickDelete().confrm_delete();
+		String actualResult = accP.getdelete_msg();
+		System.out.println("Actual Result: " + actualResult);
+		Assert.assertEquals(actualResult, accountName);
+		
+	}
+}
