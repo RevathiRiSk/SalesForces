85408580b4a1028b92faf9281ff195fe159495d4
RevathiRiSk <140264157+RevathiRiSk@users.noreply.github.com>
2023-10-03 17:29:11 -0400

    Update AccountsPage.java

--- src/main/java/pages/AccountsPage.java
+++ src/main/java/pages/AccountsPage.java
@@ -27,14 +27,26 @@ public class AccountsPage extends Sales_BaseClass {
 	@FindBy(xpath = "//input[contains(@aria-label,'Search Recently')]")
 	private WebElement inputSearch;
 	
-	@FindBy(xpath = "//table[@aria-label='Recently Viewed']/tbody/tr[1]/td[6]")
+	@FindBy(xpath = "//table[@aria-label='Recently Viewed']/tbody//tr[1]//td[6]//a")
 	private WebElement btn_dropdown;
 	
-	@FindBy(xpath = "//div[@class='forceActionLink']")
+	@FindBy(xpath = "//a[@title='Edit']")
 	private WebElement click_edit;
 	
-	@FindBy(xpath = "//span[contains(@class,'forceOutputPhone')]")
+	@FindBy(xpath = "//a[@title='Delete']")
+	private WebElement click_delete;
+	
+	@FindBy(xpath = "//table[@aria-label='Recently Viewed']/tbody//tr[1]/td[4]//span")
 	private WebElement txtFirstPhoneNo;
+	
+	@FindBy(xpath = "//button[@title='Delete']")
+	private WebElement btn_delete;
+	
+	@FindBy(xpath = "//th[@aria-label='Account Name']//a")
+	private WebElement acc_nameSort;
+	
+	@FindBy(xpath = "//span[text()='No items to display.']")
+	private WebElement getDelete_msg;
 
 	public CreateAccountPage clickOnNew() {
 		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(btn_new));
@@ -45,23 +57,41 @@ public class AccountsPage extends Sales_BaseClass {
 
 	}
 	
-	public AccountsPage inputInSearch(String accountName) {
+	public AccountsPage inputInSearch(String accountName) throws InterruptedException {
 		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(inputSearch));
-		inputSearch.sendKeys(accountName , Keys.ENTER);
+		inputSearch.sendKeys(accountName);
+		inputSearch.sendKeys(Keys.ENTER);
+		Thread.sleep(5000);
 		return this;
 	}
 	
 	public AccountsPage clickDropDown() {
-		btn_dropdown.click();
+	//	((JavascriptExecutor) driver).executeScript("arguments[0].click();", btn_dropdown);
+	//	new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(btn_dropdown));
+		driver.findElement(By.xpath("//table[@aria-label='Recently Viewed']/tbody//tr[1]//td[6]//a")).click();
+		//btn_dropdown.click();
 		return this;
 	}
 	
-	public EditPage clickEdit() {
+	public EditAccountPage clickEdit() {
 		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_edit));
 		click_edit.click();
-		EditPage editP = new EditPage(driver);
+		EditAccountPage editP = new EditAccountPage(driver);
 		PageFactory.initElements(driver, editP);
 		return editP;
+	}
+	
+	public AccountsPage clickDelete() {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_delete));
+		click_delete.click();
+		return this;
+	}
+	
+	public AccountsPage confrm_delete() throws InterruptedException {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(btn_delete));
+		Thread.sleep(3000);
+		btn_delete.click();
+		return this;
 	}
 	
 	public String getLastCreatedAccountName() {
@@ -70,5 +100,14 @@ public class AccountsPage extends Sales_BaseClass {
 	
 	public String getFirstPhoneNumber() {
 		return txtFirstPhoneNo.getText();
+	}
+	
+	public AccountsPage accNameSorting() {
+		acc_nameSort.click();
+		return this;
+	}
+	
+	public String getdelete_msg() {
+		return getDelete_msg.getText();
 	}
 }
