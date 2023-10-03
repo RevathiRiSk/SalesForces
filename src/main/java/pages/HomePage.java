2b3ad4fba31e0664a3cc8a45af9785e9e1e84685
RevathiRiSk <140264157+RevathiRiSk@users.noreply.github.com>
2023-10-03 17:32:36 -0400

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,9 +1,13 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends Sales_BaseClass {
 
@@ -20,14 +24,15 @@ public class HomePage extends Sales_BaseClass {
 	private WebElement btn_view;
 
 	public HomePage clickOnToggle() {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(btn_menu));
 		btn_menu.click();
 		return this;
 
 	}
 
 	public AppLauncherPage clickOnViewAll() {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(btn_view));
 		btn_view.click();
-	
 		AppLauncherPage appP = new AppLauncherPage(driver);
 		PageFactory.initElements(driver, appP);
 		return appP;
