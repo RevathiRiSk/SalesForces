ee062ed35b61927aa09fe5a814a71a03ada14586
RevathiRiSk <140264157+RevathiRiSk@users.noreply.github.com>
2023-10-03 17:31:01 -0400

    Update EditPage.java

--- src/main/java/pages/EditPage.java
+++ src/main/java/pages/EditPage.java
@@ -2,30 +2,27 @@ package pages;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class EditPage extends Sales_BaseClass {
+public class EditAccountPage extends Sales_BaseClass {
 
 
-	public EditPage(WebDriver driver) {
+	public EditAccountPage(WebDriver driver) {
 		super(driver);
 		
 	}
+
 	@FindBy(xpath = "//button[contains(@aria-label, 'Type')]")
-	private WebElement click_type;
-	
-	@FindBy(xpath = "//span[text()='Technology Partner']")
-	private WebElement select_type;
+	private WebElement lblType;
 	
 	@FindBy(xpath = "//button[contains(@aria-label, 'Industry')]")
-	private WebElement click_industry;
-	
-	@FindBy(xpath = "//span[text()='Healthcare']")
-	private WebElement select_industry;
+	private WebElement lblIndustry;
 	
 	@FindBy(xpath = "//textarea[@name='street']")
 	private WebElement input_billingAddress;
@@ -34,113 +31,96 @@ public class EditPage extends Sales_BaseClass {
 	private WebElement input_shippingAddress;
 	
 	@FindBy(xpath = "//button[contains(@aria-label, 'Customer Priority')]")
-	private WebElement click_priority;
-	
-	@FindBy(xpath = "//span[text()='Low']")
-	private WebElement select_priority;
+	private WebElement lblPriority;
 	
 	@FindBy(xpath = "//button[contains(@aria-label, 'SLA')]")
-	private WebElement click_SLA;
-	
-	@FindBy(xpath = "//span[text()='Silver']")
-	private WebElement select_SLA;
+	private WebElement lbl_SLA;
 	
 	@FindBy(xpath = "//button[contains(@aria-label, 'Active')]")
-	private WebElement click_active;
-	
-	@FindBy(xpath = "(//span[text()='No'])[3]")
-	private WebElement select_active;
+	private WebElement lbl_active;
 	
 	@FindBy(xpath = "//input[@name='Phone']")
 	private WebElement inputPhone;
 	
 	@FindBy(xpath = "//button[contains(@aria-label, 'Upsell Opportunity')]")
-	private WebElement click_opportunity;
-	
-	@FindBy(xpath = "//span[text()='No']")
-	private WebElement select_opportunity;
+	private WebElement lbl_opportunity;
 	
 	@FindBy(xpath = "//button[text()='Save']")
 	private WebElement btn_save;
+	
+	
+	
+	public EditAccountPage selectType(String input) {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(lblType));
+		lblType.click();
+		String xpathInput = "//span[text()='"+input+"']";
+		driver.findElement(By.xpath(xpathInput)).click();
+		return this;
+	}
+	
 
-	
-	public EditPage clickType() {
-		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_type));
-		click_type.click();
+	public EditAccountPage selectIndustry(String input) {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(lblIndustry));
+		lblIndustry.click();
+		String xpathInput = "//span[text()='"+input+"']";
+		driver.findElement(By.xpath(xpathInput)).click();
 		return this;
+		
 	}
 	
-	public EditPage selectType() {
-		select_type.click();
-		return this;
-	}
-	
-	public EditPage clickIndustry() {
-		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_industry));
-		click_industry.click();
-		return this;
-	}
-	
-	public EditPage selectIndustry() {
-		select_industry.click();
-		return this;
-	}
-	
-	public EditPage enterBillingAddress(String address) {
+	public EditAccountPage enterBillingAddress(String address) {
 		input_billingAddress.sendKeys(address);
 		return this;
 	}
 	
-	public EditPage enterShippingAddress(String address) {
+	public EditAccountPage enterShippingAddress(String address) {
 		input_shippingAddress.sendKeys(address);
 		return this;
 	}
 	
-	public EditPage clickPriority() {
-		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_priority));
-		click_priority.click();
+	
+	public EditAccountPage selectPriority(String input) {
+		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(lblPriority));
+		lblPriority.click();
+		String xpathInput = "//span[text()='"+input+"']";
+		driver.findElement(By.xpath(xpathInput)).click();
 		return this;
 	}
 	
-	public EditPage selectPriority() {
-		select_priority.click();
+	
+	public EditAccountPage selectSLA(String input) {
+		lbl_SLA.click();
+		String xpathInput = "//span[text()='"+input+"']";
+		driver.findElement(By.xpath(xpathInput)).click();
+		return this;
+		
+	}
+	
+	
+	public EditAccountPage selectActive(String input) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", lbl_active);
+		lbl_active.click();
+		String xpathInput = "(//span[text()='"+input+"'])[3]";
+		driver.findElement(By.xpath(xpathInput)).click();
 		return this;
 	}
 	
-	public EditPage clickSLA() {
-		click_SLA.click();
-		return this;
-	}
-	
-	public EditPage selectSLA() {
-		select_SLA.click();
-		return this;
-	}
-	
-	public EditPage clickActive() {
-		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_active));
-		click_active.click();
-		return this;
-	}
-	
-	public EditPage selectActive() {
-		select_active.click();
-		return this;
-	}
-	
-	public EditPage enterPhone(String number) {
+	public EditAccountPage enterPhone(String number) {
+		inputPhone.clear();
 		inputPhone.sendKeys(number);
 		return this;
 	}
 	
-	public EditPage clickOpportunity() {
-		new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(click_opportunity));
-		click_opportunity.click();
+	public EditAccountPage clickOpportunity() {
+		
 		return this;
 	}
 	
-	public EditPage selectOpportunity() {
-		select_opportunity.click();
+	public EditAccountPage selectOpportunity(String input) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", lbl_opportunity);
+		lbl_opportunity.click();
+		String xpathInput = "//span[text()='"+input+"']";
+		driver.findElement(By.xpath(xpathInput)).click();
 		return this;
 	}
 	
