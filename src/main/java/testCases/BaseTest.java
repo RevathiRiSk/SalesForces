43e101cd02f7c2587f924c711c4a41d15cf3b2c2
RevathiRiSk <140264157+RevathiRiSk@users.noreply.github.com>
2023-10-03 15:14:59 -0400

    Update BaseTest.java

--- src/main/java/testCases/BaseTest.java
+++ src/main/java/testCases/BaseTest.java
@@ -11,6 +11,14 @@ import org.testng.annotations.BeforeMethod;
 public class BaseTest {
 
 	public static WebDriver driver;
+	public String filename;
+	protected HomePage homeP;
+
+	@DataProvider(name = "fetchdata")
+	public String[][] sendData() {
+		String[][] readExcel = ExcelUtility.readExcelData(filename);
+		return readExcel;
+	}
 
 	@BeforeMethod
 	public void preConditions() {
@@ -22,11 +30,26 @@ public class BaseTest {
 		driver.get("https://login.salesforce.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
+		LoginPage loginP = LoginPage.initialize(driver);
+		homeP= loginP.loginWithValidCredentials("revmohan@gmail.com", "Mohanrevathi1!");
+	
 	}
 
 	@AfterMethod
+
+	public void afterMethod(ITestResult result) throws IOException {
+
+		if (!result.isSuccess()) {
+			TakesScreenshot screenShot = (TakesScreenshot) driver;
+			File src = screenShot.getScreenshotAs(OutputType.FILE);
+			File destination = new File("./images/failures.png");
+			FileUtils.copyFile(src, destination);
+
+		}
+	}
+
 	public void postConditions() {
 		driver.quit();
 	}
+
 }
